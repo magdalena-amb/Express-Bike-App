87351cbf0ad02501d6cfbf4b0e32c2c22453768a
Magda Ambrozy <magda.ambrozy@gmail.com>
2019-10-03 21:53:24 +0200

    Add files

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID' : '411941747996-a0tq9l640nioptjo1n8qbb4udh35s7bv.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET' : 'nPNj0k2u1ADa4EGBZrdOoXTw',
+    'CALLBACK_URL' : 'http://localhost:3000/auth/google/callback'
+};
---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const config = require('./config');
+const app = express();
+let googleProfile = {};
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+passport.deserializeUser((obj, done) => {
+    done(null, obj);
+});
+
+passport.use(new GoogleStrategy({
+    clientID: config.GOOGLE_CLIENT_ID,
+    clientSecret:config.GOOGLE_CLIENT_SECRET,
+    callbackURL: config.CALLBACK_URL
+},
+(accessToken, refreshToken, profile, cb) => {
+    googleProfile = {
+        id: profile.id,
+        displayName: profile.displayName
+    };
+    cb(null, profile);
+}
+));
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.use(express.static('assets'));
+app.use(passport.initialize());
+app.use(passport.session());
+
+//app routes
+app.get('/', (req, res) => {
+    res.render('index', { user: req.user });
+});
+
+app.get('/logged', (req, res) => {
+    res.render('logged', { user: googleProfile });
+});
+
+//Passport routes
+app.get('/auth/google',
+passport.authenticate('google', {
+scope : ['profile', 'email']
+}));
+
+app.get('/auth/google/callback',
+    passport.authenticate('google', {
+        successRedirect : '/logged',
+        failureRedirect: '/'
+    }));
+
+app.listen(3000);
